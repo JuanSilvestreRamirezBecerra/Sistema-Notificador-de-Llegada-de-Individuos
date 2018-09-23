fa03548e88b3b5e5ea354e396a3d54dd8dded3d6
JuanSilvestreRamirezBecerra <trebol_silvestre14@hotmail.com>
2018-09-23 15:48:50 -0500

    Se añadio la funcion de ahorro de energia y un led para demostrarlo

--- PuertaAlarma.ino
+++ PuertaAlarma.ino
@@ -1,24 +1,41 @@
+#include <avr/sleep.h>
 #define buzzer 11//pin donde esta la salida para el buzzer
 #define puerta 3//pin que recibe la señal de la entrada
+#define led 12//Luz para ver modoSleep
 
 volatile int estadoPuerta = LOW;//El estado de la puerta es cerrado
 
 void setup()
 {
   pinMode(buzzer, OUTPUT);//Se define el buzzer como pin de salida
+  pinMode(led,OUTPUT);//Se define el led con un pin de salida
   pinMode(puerta, INPUT); //Se define el pin puerta como entrada
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);//Establecemos el modo de bajo consumo.
   attachInterrupt(digitalPinToInterrupt(puerta), interrupcion, RISING); //Se crea una interrupcion con el pin puerta
+  Serial.begin(9600);
 }
 
 void loop()
 {
+  if(estadoPuerta==LOW){
+     digitalWrite(led,LOW);//Apaga el led con la alarma
+     sleep_enable();//inicializamos el modo bajo consumo
+     sleep_mode();//ponemos el modo bajo consumo.
+
+  }else{
+     sleep_disable();//desactivamos el modo bajo consumo
+     digitalWrite(led,HIGH);//enciende el led con la alrma
+     alarma(estadoPuerta);//Manda el estado de la puerta a la alarma
+  }
+   
   
-  alarma(estadoPuerta);//Manda el estado de la puerta a la alarma
+ 
 }
 
 void interrupcion() {
   estadoPuerta = !estadoPuerta;//Si se irrumpe la puerta, hay un cambio de estado de cerrado a abierto y viceversa
 }
+
 
 void alarma(int estado)
 {
