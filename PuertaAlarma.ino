e2ea606396aafe79822fe3d84f41191004ed5d33
JuanSilvestreRamirezBecerra <trebol_silvestre14@hotmail.com>
2018-09-23 12:47:16 -0500

    Se añadio un reed switch para usar como bandera

--- PuertaAlarma.ino
+++ PuertaAlarma.ino
@@ -1,56 +1,69 @@
 #define buzzer 11
+#define puerta 8
 
-
-
-void setup() {
-pinMode(buzzer, OUTPUT);
+void setup() 
+{
+  pinMode(buzzer, OUTPUT);
 }
 
-void loop() {
- 
-alarma();
-}
-
-void alarma(){
+void loop() 
+{
   
-    for(int x=0;x<7;x++){
-      
-     analogWrite(buzzer,128); //emite sonido
-     delay(200); //espera
-     digitalWrite(buzzer, LOW); //deja de emitir
-     delay(200);//espera 
+  if(digitalRead(puerta)==LOW){
+    }
+  else
+  {
+    alarma();
   }
-  for(int z=0;z<5;z++){
-    analogWrite(buzzer,128); //emite sonido
-     delay(100); //espera medio segundo
-         digitalWrite(buzzer, LOW); //deja de emitir
-     delay(100);//espera medio segundo
-          analogWrite(buzzer,128); //emite sonido
-     delay(100); //espera medio segundo
-         digitalWrite(buzzer, LOW); //deja de emitir
-     delay(100);//espera medio segundo
-          analogWrite(buzzer,128); //emite sonido
-     delay(100); //espera medio segundo
-         digitalWrite(buzzer, LOW); //deja de emitir
-     delay(100);//espera medio segundo
-          analogWrite(buzzer,128); //emite sonido
-          
-     delay(100); //espera medio segundo
-         digitalWrite(buzzer, LOW); //deja de emitir
-     delay(100);//espera medio segundo
-          analogWrite(buzzer,128); //emite sonido
-     delay(100); //espera medio segundo
-         digitalWrite(buzzer, LOW); //deja de emitir
-     delay(100);//espera medio segundo
-          analogWrite(buzzer,128); //emite sonido
-     delay(100); //espera medio segundo
-         digitalWrite(buzzer, LOW); //deja de emitir
-     delay(100);//espera medio segundo
-          analogWrite(buzzer,128); //emite sonido
-          
-     delay(300); //espera medio segundo
-         digitalWrite(buzzer, LOW); //deja de emitir
-     delay(100);//espera medio segundo
-  
+
 }
+
+void alarma()
+{
+  
+    for(int x=0;x<7;x++)
+    {
+      
+      analogWrite(buzzer,128); //emite sonido
+      delay(200); //espera
+      digitalWrite(buzzer, LOW); //deja de emitir
+      delay(200);//espera 
+    }
+  for(int z=0;z<5;z++)
+    {
+      analogWrite(buzzer,128); //emite sonido
+      delay(100); //espera 
+      digitalWrite(buzzer, LOW); //deja de emitir
+      delay(100);//espera 
+      
+      analogWrite(buzzer,128); //emite sonido
+      delay(100); //espera 
+      digitalWrite(buzzer, LOW); //deja de emitir
+      delay(100);//espera 
+     
+      analogWrite(buzzer,128); //emite sonido
+      delay(100); //espera 
+      digitalWrite(buzzer, LOW); //deja de emitir
+      delay(100);//espera 
+     
+      analogWrite(buzzer,128); //emite sonido
+      delay(100); //espera 
+      digitalWrite(buzzer, LOW); //deja de emitir
+      delay(100);//espera 
+     
+      analogWrite(buzzer,128); //emite sonido
+      delay(100); //espera 
+      digitalWrite(buzzer, LOW); //deja de emitir
+      delay(100);//espera 
+     
+      analogWrite(buzzer,128); //emite sonido
+      delay(100); //espera 
+      digitalWrite(buzzer, LOW); //deja de emitir
+      delay(100);//espera 
+     
+      analogWrite(buzzer,128); //emite sonido
+      delay(300); //espera 
+      digitalWrite(buzzer, LOW); //deja de emitir
+      delay(100);//espera
+    }
 }
