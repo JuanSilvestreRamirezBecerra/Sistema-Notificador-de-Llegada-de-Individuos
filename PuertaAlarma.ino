3cece8b523d669e24a6858ba5ec94288cbf580ec
JuanSilvestreRamirezBecerra <trebol_silvestre14@hotmail.com>
2018-09-23 14:34:06 -0500

    Se comento el codigo

--- PuertaAlarma.ino
+++ PuertaAlarma.ino
@@ -1,69 +1,32 @@
-#define buzzer 11
-#define puerta 8
+#define buzzer 11//pin donde esta la salida para el buzzer
+#define puerta 3//pin que recibe la señal de la entrada
 
-void setup() 
+volatile int estadoPuerta = LOW;//El estado de la puerta es cerrado
+
+void setup()
 {
-  pinMode(buzzer, OUTPUT);
+  pinMode(buzzer, OUTPUT);//Se define el buzzer como pin de salida
+  pinMode(puerta, INPUT); //Se define el pin puerta como entrada
+  attachInterrupt(digitalPinToInterrupt(puerta), interrupcion, RISING); //Se crea una interrupcion con el pin puerta
 }
 
-void loop() 
+void loop()
 {
   
-  if(digitalRead(puerta)==LOW){
-    }
-  else
-  {
-    alarma();
+  alarma(estadoPuerta);//Manda el estado de la puerta a la alarma
+}
+
+void interrupcion() {
+  estadoPuerta = !estadoPuerta;//Si se irrumpe la puerta, hay un cambio de estado de cerrado a abierto y viceversa
+}
+
+void alarma(int estado)
+{
+  if (estadoPuerta == LOW) {//Si el estado de la puerta es cerrado no pasa nada
+  } else {//Si el estado de la puerta es abierto suena la alarma de seguridad
+    analogWrite(buzzer, 500); //emite sonido
+    delay(200); //espera
+    digitalWrite(buzzer, LOW); //deja de emitir
+    delay(100);//espera
   }
-
-}
-
-void alarma()
-{
-  
-    for(int x=0;x<7;x++)
-    {
-      
-      analogWrite(buzzer,128); //emite sonido
-      delay(200); //espera
-      digitalWrite(buzzer, LOW); //deja de emitir
-      delay(200);//espera 
-    }
-  for(int z=0;z<5;z++)
-    {
-      analogWrite(buzzer,128); //emite sonido
-      delay(100); //espera 
-      digitalWrite(buzzer, LOW); //deja de emitir
-      delay(100);//espera 
-      
-      analogWrite(buzzer,128); //emite sonido
-      delay(100); //espera 
-      digitalWrite(buzzer, LOW); //deja de emitir
-      delay(100);//espera 
-     
-      analogWrite(buzzer,128); //emite sonido
-      delay(100); //espera 
-      digitalWrite(buzzer, LOW); //deja de emitir
-      delay(100);//espera 
-     
-      analogWrite(buzzer,128); //emite sonido
-      delay(100); //espera 
-      digitalWrite(buzzer, LOW); //deja de emitir
-      delay(100);//espera 
-     
-      analogWrite(buzzer,128); //emite sonido
-      delay(100); //espera 
-      digitalWrite(buzzer, LOW); //deja de emitir
-      delay(100);//espera 
-     
-      analogWrite(buzzer,128); //emite sonido
-      delay(100); //espera 
-      digitalWrite(buzzer, LOW); //deja de emitir
-      delay(100);//espera 
-     
-      analogWrite(buzzer,128); //emite sonido
-      delay(300); //espera 
-      digitalWrite(buzzer, LOW); //deja de emitir
-      delay(100);//espera
-    }
 }
